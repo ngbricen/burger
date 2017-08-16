d153321760124be55d4f165003265ba8ffb9fe40
ngbricen <bricen@gmail.com>
2017-08-16 05:54:54 -0400

    Working app with no styling

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 CREATE TABLE `burgers` (
   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
   `burger_name` VARCHAR( 255) NOT NULL,
-  `devoured` BOOLEAN NOT NULL,
+  `devoured` BOOLEAN DEFAULT false,
   `date` TIMESTAMP NOT NULL,
   PRIMARY KEY ( `id` ) 
 );
