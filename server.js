2315d8c96fe6e156a5f23ae1f14ea39bb74005d9
ngbricen <bricen@gmail.com>
2017-08-16 22:59:38 -0400

    Making the Port number generic for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var port = proces.event.PORT || 3000;
 
 var app = express();
 
