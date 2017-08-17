8cdcd216879874c428e228fde396aacaffb32602
ngbricen <bricen@gmail.com>
2017-08-16 23:46:53 -0400

    Adjusting solution for Heroku Deployment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var app = express();
 
 var port = process.event.PORT || 3000;
 
-var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
