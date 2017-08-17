8de5c35c118cf7add07bfee213aacaf212337651
ngbricen <bricen@gmail.com>
2017-08-17 15:10:34 -0400

    Adding styling and adjustments to DB

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,15 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+router.get("/api/burgers", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.json(hbsObject);
+  });
+});
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name,devoured) VALUES ('Double Up', 1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double Up', 0);
 INSERT INTO burgers (burger_name,devoured) VALUES ('Cool Beans',0);
-INSERT INTO burgers (burger_name,devoured) VALUES ('Peanut Dream',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Peanut Dream',0);
